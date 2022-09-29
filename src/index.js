99df189348a5c15557d9be8acd88917e7f827294
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-09-29 18:48:50 +0300

    working on signup page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import  {DbContext}  from './Context/DBContext';
+import{db , auth} from 'firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <DbContext.Provider value = {{db , auth}}>
     <App />
-  </React.StrictMode>
+  </DbContext.Provider>
 );
 
 
