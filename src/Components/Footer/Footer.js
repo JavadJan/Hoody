973dbdfbd401a446a1868eb5f298d5bae8e09dbe
Sirine Saoudi <95916760+Sirine54@users.noreply.github.com>
2022-10-09 23:39:35 +0200

    add footer and style it

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 
        /* REACT ICONS */
 import {FaFacebookF} from 'react-icons/fa';
-import {BsTwitter,BsInstagram}from 'react-icons/bs';
+import {BsTwitter,BsInstagram,BsSuitHeartFill}from 'react-icons/bs';
 import {ImPinterest2} from 'react-icons/im'
 
 export const Footer = () => {
@@ -14,6 +14,7 @@ export const Footer = () => {
                 <div className="logo">HOoDY</div>
             </div>
           <div className="footer2">
+            <h3>Menu</h3>
                 <Link>Home</Link>
                 <Link>About</Link>
                 <Link>Service</Link>
@@ -21,10 +22,16 @@ export const Footer = () => {
                 <Link>Subscribe</Link>       
           </div>
           <div className="footer3">
+              <h3>
+                contact us
+              </h3>
+              <span>+123 456 789</span>
+              <span>email@domain.com</span>
+              <span>location</span>
 
           </div>
           <div className="footer4">
-            <h4>Check our social activities</h4>
+            <h3>Check our social activities</h3>
             <ul className="social-network">
             <li><a href="#" target="_blank"><FaFacebookF></FaFacebookF></a></li>
             <li><a href=""><BsTwitter></BsTwitter> </a></li>
@@ -32,6 +39,9 @@ export const Footer = () => {
             <li><a href=""><ImPinterest2></ImPinterest2></a></li>
             </ul>
           </div>
+            <div class="footer-copyRight">
+            <small>Copyright © 2012 -{new Date().getFullYear()} all right reserved <span>HOODY</span></small>
+            </div>
         </footer>
 
     )
