9f884b9177608ada6fd78419d5ad4a973c8fdb18
Sirine Saoudi <95916760+Sirine54@users.noreply.github.com>
2022-10-12 23:55:47 +0200

    add media queries and change colors

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -64,6 +64,7 @@ export function Login() {
         setEmail('')
         setPassword('')
         setError('unsuccessful to register! ')
+      
       }
     }
     else {
@@ -87,7 +88,7 @@ export function Login() {
 
 
   //handle facebook sign up
-  const hanldeFacebookSignUp = async () => {
+  const handleFacebookSignUp = async () => {
     const provider = new FacebookAuthProvider()
     await signInWithPopup(auth, provider)
     await addDoc(collection(db, 'users'), {
@@ -174,6 +175,7 @@ export function Login() {
               <input value={password} onChange={({ target }) => { setPassword(target.value) }} type="password" placeholder="Password" />
             </div>
 
+
             <button type="submit" className="btn">Sign up</button>
 
             <p className="social-text">Or Sign up with social platforms</p>
@@ -181,7 +183,7 @@ export function Login() {
               <Link href="#" className="social-icon" onClick={handleGoogleSignUp}>
                 <i className="fab fa-google"></i>
               </Link>
-              <Link href="#" className="social-icon" onClick={hanldeFacebookSignUp}>
+              <Link href="#" className="social-icon" onClick={handleFacebookSignUp}>
                 <i className="fab fa-facebook-f"></i>
               </Link>
               <Link href="#" className="social-icon" onClick={handleTwitterSignUp}>
