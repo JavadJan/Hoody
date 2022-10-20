bb297778512f9ab6f22cbd46adb6e689c0bccc4e
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-10-20 10:26:07 +0300

    working on categories

--- src/Components/Pages/Dashboard/OthersItems.js
+++ src/Components/Pages/Dashboard/OthersItems.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export const OthersItems = () => {
-  return (
-    <div className='others-items'>OthersItems</div>
-  )
+    return (
+        <div className='others-items'>
+            <div className='location'>
+                <div className='your-location'>
+                    <i className="fas fa-location"></i>
+                </div>
+                <div className="search-box-loc">
+                    <input type="text" placeholder='Search location ...' Style="font-family:Arial, FontAwesome" />
+                    {/* placeholder="&#xF002;" */}
+                    <span htmlFor="" className="search-icon">
+                        <i className="fas fa-search"></i>
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    )
 }
