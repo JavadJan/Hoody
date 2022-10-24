d4caa649f5bc5756ca5b9ab8ff5e9a558e6037b3
Mansurman32 <mensurkedir@outlook.com>
2022-10-24 09:51:54 -0700

    User profile content modified and Pin hover effect added

--- src/Components/UserAccount/Pin.js
+++ src/Components/UserAccount/Pin.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './Pin.css';
+import "./Pin.css";
 
 // function upload_img(event, setPinImage) {
 //     if (event.target.files && event.target.files[0]) {
@@ -17,61 +17,67 @@ import './Pin.css';
 // }
 
 function check_size(event) {
-    const image = event.target;
+  const image = event.target;
 
-    image.classList.add('pin_max_width');
+  image.classList.add("pin_max_width");
 
-    if (
-        image.getBoundingClientRect().width < image.parentElement.getBoundingClientRect().width ||
-        image.getBoundingClientRect().height < image.parentElement.getBoundingClientRect().height
-    ) {
-        image.classList.remove('pin_max_width');
-        image.classList.add('pin_max_height');
-    }
+  if (
+    image.getBoundingClientRect().width <
+      image.parentElement.getBoundingClientRect().width ||
+    image.getBoundingClientRect().height <
+      image.parentElement.getBoundingClientRect().height
+  ) {
+    image.classList.remove("pin_max_width");
+    image.classList.add("pin_max_height");
+  }
 
-    image.style.opacity = 1;
+  image.style.opacity = 1;
 }
 
 function Pin(props) {
-    // const [pinImage, setPinImage] = useState();
+  // const [pinImage, setPinImage] = useState();
 
-    return (
-        // <div>
-        //     <input onChange={event => upload_img(event, setPinImage)} type="file" name="picture" id="picture" value="" />
+  return (
+    <div className={`card card_${props.pinDetails.pin_size}`}>
+      <div className="pin_title">{props.pinDetails.title}</div>
 
-        //     <div className="card">
-        <div className={`card card_${props.pinDetails.pin_size}`}>
-            <div className="pin_title">{props.pinDetails.title}</div>
-
-            <div className="pin_modal">
-                <div className="modal_head">
-                    <div className="save_card">Save</div>
-                </div>
-
-                <div className="modal_foot">
-                    <div className="destination">
-                        <div className="pint_mock_icon_container">
-                            <img src="./images/upper-right-arrow.png" alt="destination" className="pint_mock_icon" />
-                        </div>
-                        <span>{props.pinDetails.destination}</span>
-                    </div>
-
-                    <div className="pint_mock_icon_container">
-                        <img src="./images/send.png" alt="send" className="pint_mock_icon" />
-                    </div>
-
-                    <div className="pint_mock_icon_container">
-                        <img src="./images/ellipse.png" alt="edit" className="pint_mock_icon" />
-                    </div>
-                </div>
-            </div>
-
-            <div className="pin_image">
-                <img onLoad={check_size} src={props.pinDetails.img_blob} alt="pin_image" />
-            </div>
+      <div className="pin_modal">
+        <div className="modal_head">
+          <i class="fa-solid fa-heart"></i>
+          <i class="fa-solid fa-share"></i>
+          <i class="fa-solid fa-download"></i>
         </div>
-        // </div>
-    )
+
+        <div className="modal_foot">
+          <div className="description">
+            <span>{props.pinDetails.description}</span>
+          </div>
+          <div className="destination">
+            {/* <div className="pint_mock_icon_container">
+              <i class="fa-solid fa-up-right" id="pint-destination-icon"></i>
+            </div> */}
+            <span>{`${"Owner"} ${props.pinDetails.destination}`}</span>
+          </div>
+          <div className="wrapper">
+            <div className="cart-btn">
+              <div className="icon">
+                <i class="fa-solid fa-cart-shopping"></i>
+              </div>
+              <span>Add To Cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="pin_image">
+        <img
+          onLoad={check_size}
+          src={props.pinDetails.img_blob}
+          alt="pin_image"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Pin;
--- src/Components/UserAccount/UserProfile.js
+++ src/Components/UserAccount/UserProfile.js
@@ -1,111 +1,105 @@
-import React, {useEffect, useState} from 'react'
-import './UserProfile.css';
-import Pin from './Pin.js';
-import Model from './Model.js';
+import React, { useEffect, useState } from "react";
+import "./UserProfile.css";
+import Pin from "./Pin.js";
+import Model from "./Model.js";
+import Plus from '../../assets/plus.png';
 
 class UserProfile extends React.Component {
-    // useEffect(()=>{
-    //     const allIcon = document.querySelectorAll('.icon-container');
-  
-    //     function setMenuActive(){
-    //         allIcon.forEach((e)=> e.classList.remove('active'));
-    //         this.classList.add('active');
-    //     }
-    //     allIcon.forEach(e=> e.addEventListener('click', setMenuActive))
-    // },[]);
-    
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        pins: [],
-        show_modal: false,
-      };
-    }
-    
-    add_pin = (pinDetails) => {
-      this.setState((_state) => {
-        const new_pins = [..._state.pins];
-  
-        new_pins.push(<Pin pinDetails={pinDetails} key={_state.pins.length} />);
-  
-        return {
-          pins: new_pins,
-          show_modal: false,
-        };
-      });
+  // useEffect(()=>{
+  //     const allIcon = document.querySelectorAll('.icon-container');
+
+  //     function setMenuActive(){
+  //         allIcon.forEach((e)=> e.classList.remove('active'));
+  //         this.classList.add('active');
+  //     }
+  //     allIcon.forEach(e=> e.addEventListener('click', setMenuActive))
+  // },[]);
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pins: [],
+      show_modal: false,
     };
-    render() {
-      return (
-        <div>
-          <div className="profile-container">
-            <div className="menu-container">
-              <div className="sub-menu">
-                <div className="icon-container">
-                  <i className="fa-regular fa-user"></i>
-                </div>
-                <div className="icon-container">
-                  <i className="fa-regular fa-bell"></i>
-                </div>
-                <div className="icon-container">
-                  <i className="fa-regular fa-comment-dots"></i>
-                </div>
-                <div className="icon-container">
-                  <i className="fa-regular fa-heart"></i>
-                </div>
-                <div className="icon-container">
-                  <i
-                    className="fa-regular fa-plus pint_mock_icon_container add_pin"
-                    onClick={() => this.setState({ show_modal: true })}
-                  ></i>
-                </div>
-                <div className="icon-container">
-                  <i className="fa-regular fa-question"></i>
-                </div>
-              </div>
-            </div>
-            <main>
-              <div className="box">
-                <div className="search-box">
-                  <input type="text" placeholder="Search..." />
-                  <label htmlFor="" className="icon">
-                    <i className="fas fa-search"></i>
-                  </label>
-                </div>
-              </div>
-              <div className="main-container">
-                <div className="pin_container">{this.state.pins}</div>
-              </div>
-            </main>
-          </div>
-          {/* <div className="navigation_bar">
-            <div
-              onClick={() => this.setState({ show_modal: true })}
-              className="pint_mock_icon_container add_pin"
-            >
-              <img
-                src="./images/add.png"
-                alt="add_pin"
-                className="pint_mock_icon"
-              />
-            </div>
-          </div> */}
-  
-          {/* <div className="pin_container">{this.state.pins}</div> */}
-  
-          <div
-            onClick={(event) =>
-              event.target.className === "add_pin_modal"
-                ? this.setState({ show_modal: false })
-                : null
-            }
-            className="add_pin_modal_container"
-          >
-            {this.state.show_modal ? <Model add_pin={this.add_pin} /> : null}
-          </div>
-        </div>
-      );
-    }
   }
 
-export default UserProfile
+  add_pin = (pinDetails) => {
+    this.setState((_state) => {
+      const new_pins = [..._state.pins];
+
+      new_pins.push(<Pin pinDetails={pinDetails} key={_state.pins.length} />);
+
+      return {
+        pins: new_pins,
+        show_modal: false,
+      };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="profile-container">
+          <div className="menu-container">
+            <div className="sub-menu">
+              <div className="icon-container">
+                <i className="fa-regular fa-user"></i>
+              </div>
+              <div className="icon-container">
+                <i className="fa-regular fa-bell"></i>
+              </div>
+              <div className="icon-container">
+                <i className="fa-regular fa-comment-dots"></i>
+              </div>
+              <div className="icon-container">
+                <i className="fa-regular fa-heart"></i>
+              </div>
+              <div className="icon-container">
+                <img
+                  src={Plus}
+                  alt=""
+                  className="plusIcon"
+                  onClick={() => this.setState({ show_modal: true })}
+                  className="pint_mock_icon_container add_pin"
+                />
+                {/* <i
+                    className="fa-regular fa-plus pint_mock_icon_container add_pin"
+                    onClick={() => this.setState({ show_modal: true })}
+                  ></i> */}
+              </div>
+              <div className="icon-container">
+                <i className="fa-regular fa-question"></i>
+              </div>
+            </div>
+          </div>
+          <main>
+            <div className="box">
+              <div className="search-box">
+                <input type="text" placeholder="Search..." />
+                <label htmlFor="" className="icon">
+                  <i className="fas fa-search"></i>
+                </label>
+              </div>
+            </div>
+            <div className="main-container">
+              <div className="pin_container">{this.state.pins}</div>
+            </div>
+          </main>
+        </div>
+
+        <div
+          onClick={(event) =>
+            event.target.className === "add_pin_modal"
+              ? this.setState({ show_modal: false })
+              : null
+          }
+          className="add_pin_modal_container"
+        >
+          {this.state.show_modal ? <Model add_pin={this.add_pin} /> : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
