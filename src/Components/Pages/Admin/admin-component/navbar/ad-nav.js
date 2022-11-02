653fc36f2de9887e50ccc867ce16ad90f8abd045
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-11-02 10:11:47 +0200

    finished admmin navbar

--- src/Components/Pages/Admin/admin-component/navbar/ad-nav.js
+++ src/Components/Pages/Admin/admin-component/navbar/ad-nav.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './navbar.scss'
+import { UilSearch } from '@iconscout/react-unicons'
+import { UilListUl } from '@iconscout/react-unicons'
+import { UilCommentAltMessage } from '@iconscout/react-unicons'
+import Default from "./Default.png";
+
 export const Navbar = () => {
     return (
         <div className='ad-navbar'>
-            navbar
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder='Search ... ' />
+                    <UilSearch />
+                </div>
+                <div className='ad-items'>
+                    <div className='item'>
+                        <UilCommentAltMessage />
+                        <div className='counter'>2</div>
+                    </div>
+                    <div className="item menu">
+                        <UilListUl />
+                    </div>
+                    <div className="item user">
+                        <img src={Default} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
