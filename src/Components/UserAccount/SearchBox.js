e7cacc04ca71552ecf5f05ca19c9a77ab1bf7b5f
Mansurman32 <mensurkedir@outlook.com>
2022-11-03 10:48:27 -0700

    Some features added to search filtter component

--- src/Components/UserAccount/SearchBox.js
+++ src/Components/UserAccount/SearchBox.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import './SearchBox.css';
 
 function SearchBox() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
   return (
     <div className="box">
       <div className="search-box">
