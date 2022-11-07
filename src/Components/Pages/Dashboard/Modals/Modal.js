775c7a0c24db7c2c1d011e5184d82bca63711ce9
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-11-07 07:19:39 +0200

    add photo on firebase

--- src/Components/Pages/Dashboard/Modals/Modal.js
+++ src/Components/Pages/Dashboard/Modals/Modal.js
@@ -9,15 +9,21 @@ import { UilCloudUpload } from '@iconscout/react-unicons'
 import { useUser } from '../../../../DB/useUser'
 import { userContext } from '../../../../Context/userContext'
 import { useContext } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { DbContext } from '../../../../Context/DBContext'
+import { getDownloadURL, ref, uploadBytes, uploadBytesResumable } from 'firebase/storage'
 
-export const Modal = ({ open, setOpenModal, setTurnLocation , coordination }) => {
+export const Modal = ({ open, setOpenModal, setTurnLocation, coordination }) => {
+
+  const { db, storage } = useContext(DbContext)
   const [image, setImage] = useState(null)
   const [type, setType] = useState('donate')
   const [category, setCategory] = useState(null)
   const [explain, setExplain] = useState('')
-  
+  const [getLiknDowanload , setGetLinkDownload] = useState('')
+
   const { user: { username, fullName, userId, id } } = useUser()
-  console.log(id )  
+  console.log(id)
 
   //close modal
   if (!open) return null
@@ -25,19 +31,77 @@ export const Modal = ({ open, setOpenModal, setTurnLocation , coordination }) =>
     setOpenModal(false)
   }
 
+  console.log(image, id, userId)
 
   //for saving image
-  function handleSaveImage(e) {
+  async function handleSaveImage(e) {
     e.preventDefault()
     const item = {
-      imageInfo: image,
+      imageInfo: { name: image.name, size: image.size },
       type: type,
       category: category,
-      explain:explain , 
-      coordination:coordination
+      explain: explain,
+      coordination: coordination,
+      userDocId: id
     }
     console.log(item)
-    // docId:currentUser.id,
+    console.log(`${category}/` + image.name)
+
+    //add items in fireStore
+    await addDoc(collection(db, 'items'), item)
+
+    //---------------------add picture in storage 
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, `${category}/` + image.name);
+    const uploadTask = uploadBytesResumable(storageRef, image , item)
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          setGetLinkDownload(downloadURL)
+        });
+      }
+    );
+
+
+    //<--------------------------------
+
   }
 
   console.log('coordination', coordination)
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DbContext } from './Context/DBContext';
-import { db, auth } from './DB/firebase'
+import { db, auth , storage } from './DB/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <DbContext.Provider value={{ db, auth}}>
+  <DbContext.Provider value={{ db, auth , storage}}>
     <App />
   </DbContext.Provider>
 );
